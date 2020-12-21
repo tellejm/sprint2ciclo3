2e450bec971ec204235241bca88be62086e93e3a
tellejm <61441662+tellejm@users.noreply.github.com>
2020-12-21 11:19:08 -0500

    Create userq.py

---
+++ models/userq.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+class Userq(BaseModel):
+    username:str
+    question1:int
+    question2:int
+    question3:int
+    question4:int
+    question5:int
+    question6:int
+    question7:int
+    question8:int
+    question9:int
+    question10:int
+    question11:int
+    question12:int
+    
